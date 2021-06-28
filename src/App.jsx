6d32331ea486287fdfbf7127aa3c6d1486ee9854
Raed56 <rseraj@usf.edu>
2021-06-29 05:26:32 +0600

    changed comments

--- src/App.jsx
+++ src/App.jsx
@@ -9,3 +9,13 @@ function App() {
 }
 
 export default App;
+
+/*
+1. Use media querries for all images of project.
+2 Gather high quality images for projects and edit them if needed
+3. Fix the positioning around the elements of the card body 
+so that all of them align properly wen displayed together.
+
+
+
+*/
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -52,13 +52,3 @@ export const Projects = () => {
         </div>
     );
 };
-
-/*
-1. Use media querries for all images of project.
-2 Gather high quality images for projects and edit them if needed
-3. Fix the positioning around the elements of the card body 
-so that all of them align properly wen displayed together.
-
-
-
-*/
