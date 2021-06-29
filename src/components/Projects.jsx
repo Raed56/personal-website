2471cc999f4f9c2ae831d5f05dbffc6e98953abf
Raed56 <rseraj@usf.edu>
2021-06-29 22:15:16 +0600

    optimized cards for mobile view

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,7 +22,7 @@ export const Projects = () => {
                     <WebCard
                         cardImage="https://images.unsplash.com/photo-1593642532973-d31b6557fa68?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"
                         cardTitle="Pocket"
-                        cardBody="This is a budgeting application which I built with a friend and it shows the income, expenses and the net balance. This project taught me a lot of things. Blah Blah Blah."
+                        cardBody="This is a budgeting application which I built with a friend and it shows the income, expenses and the net balance. This project taught me a lot of things."
                         languageTags={["NodeJs", "Express", "MongoDB"]}
                         liveLink="https://pocket-rn.herokuapp.com/"
                         githubLink="https://github.com/niloyKGhosh/pocket"
