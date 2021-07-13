9520f6e50a2deae7ac49d9211c6e816a6d1bf922
Raed56 <rseraj@usf.edu>
2021-07-14 03:29:57 +0600

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,16 @@ const Footer = () => {
                 <p>
                     <span>&copy; 2021 </span>Designed & Built by Raed Seraj
                 </p>
-                <p>Hosted on Netlify</p>
+                <p>
+                    Hosted on{" "}
+                    <a
+                        href="https://www.netlify.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Netlify
+                    </a>
+                </p>
             </div>
         </div>
     );
